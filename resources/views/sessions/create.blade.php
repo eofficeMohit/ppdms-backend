e3d0d126020d5c37dd452f790070e1b1ced40bff
eofficeMohit <eoffice.mohit@gmail.com>
2023-08-14 16:38:26 +0530

    updated the images/logos

--- resources/views/sessions/create.blade.php
+++ resources/views/sessions/create.blade.php
@@ -11,9 +11,7 @@
         </div>
         <main class="main-content  mt-0">
             <div class="page-header align-items-start min-vh-100"
-            style="background-image: url('/assets/img/election_dashboard.jpeg');"
-                {{-- style="background-image: url('https://images.unsplash.com/photo-1497294815431-9365093b7331?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1950&q=80');" --}}
-                >
+            style="background-image: url('/assets/img/election_dashboard.jpeg');">
                 <span class="mask opacity-6"></span>
                 <div class="container mt-5">
                     <div class="row signin-margin">
