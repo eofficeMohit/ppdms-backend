9fc06c0b0a87d8ef81a680fbd039b5d67a4dee96
rishavdeepsran <rishavdeepsran@gmail.com>
2023-08-26 14:05:17 +0530

    booth changes

---
+++ resources/views/booth/create.blade.php
@@ -0,0 +1,168 @@
+<x-layout bodyClass="g-sidenav-show  bg-gray-200">   
+    <x-navbars.sidebar activePage="booth"></x-navbars.sidebar>
+    <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
+        <!-- Navbar -->
+        <x-navbars.navs.auth titlePage="Booth Management"></x-navbars.navs.auth>
+        <!-- End Navbar -->
+        <div class="container-fluid py-4">
+            <div class="row">
+                <div class="col-md-12 mt-4">
+                    <div class="card">
+                        <div class="card-header pb-0 px-3">
+                            <h6 class="mb-0">Booth Management</h6>
+                        </div>
+                        <div class="card-body pt-4 p-3">
+                            <div class="row">
+                                <div class="col-lg-12 margin-tb">
+                                    <h3>Create New Booth</h3>
+                                        <div class="col-12 text-end">
+                                            <a class="btn btn-primary" href="{{ route('booth') }}"> Back</a>
+                                        </div>
+                                </div>
+                            </div>
+                            @if ($message = Session::get('success'))
+                                <div class="alert alert-success">
+                                    <p>{{ $message }}</p>
+                                </div>
+                            @endif
+                            @if (count($errors) > 0)
+                                <div class="alert alert-danger">
+                                    <strong>Whoops!</strong> There were some problems with your input.<br><br>
+                                    <ul>
+                                    @foreach ($errors->all() as $error)
+                                        <li>{{ $error }}</li>
+                                    @endforeach
+                                    </ul>
+                                </div>
+                            @endif
+                            {!! Form::open(array('route' => 'booth.store','method'=>'POST')) !!}
+                                <div class="row">
+                                    <div class="col-xs-6 col-sm-6 col-md-6">
+                                        <div class="form-group">
+                                            <strong>AC Code:</strong>
+                                            {!! Form::text('ac_code', null, array('placeholder' => 'ST Code','class' => 'form-control')) !!}
+                                        </div>
+                                    </div>
+                                    <div class="col-xs-6 col-sm-6 col-md-6">
+                                        <div class="form-group">
+                                            <strong>Booth Number:</strong>
+                                            {!! Form::text('booth_no', null, array('placeholder' => 'ASMB Code','class' => 'form-control')) !!}
+                                        </div>
+                                    </div>
+                                    <div class="col-xs-6 col-sm-6 col-md-6">
+                                        <div class="form-group">
+                                            <strong>Total Voters:</strong>
+                                            {!! Form::text('tot_voters', null, array('placeholder' => 'AC Type','class' => 'form-control')) !!}
+                                        </div>
+                                    </div>
+                                    <div class="col-xs-6 col-sm-6 col-md-6">
+                                        <div class="form-group">
+                                            <strong>Booth Name:</strong>
+                                            {!! Form::text('booth_name', null, array('placeholder' => 'PC Type','class' => 'form-control')) !!}
+                                        </div>
+                                    </div>
+                                    <div class="col-xs-6 col-sm-6 col-md-6">
+                                        <div class="form-group">
+                                            <strong>Booth Name Uni:</strong>
+                                            {!! Form::text('booth_name_uni', null, array('placeholder' => 'PC Number','class' => 'form-control')) !!}
+                                        </div>
+                                    </div>
+                                    <div class="col-xs-6 col-sm-6 col-md-6">
+                                        <div class="form-group">
+                                            <strong>State:</strong>
+                                            <select class="form-control" id="state_id" name="state_id">
+                                                <option value="">Select State</option>
+                                                @foreach($states as $key => $value)
+                                                    <option value="{{ $key }}">{{ $value }}</option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="col-xs-6 col-sm-6 col-md-6">
+                                        <div class="form-group">
+                                            <strong>District:</strong>
+                                            <select class="form-control" id="district_id" name="district_id">
+                                            <option value="">Select District</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="col-xs-6 col-sm-6 col-md-6">
+                                        <div class="form-group">
+                                            <strong>Assembly:</strong>
+                                            <select class="form-control" id="assembly_id" name="assembly_id">
+                                                <option value="">Select Assembly</option>
+                                                @foreach($assembly as $key => $value)
+                                                    <option value="{{ $key }}">{{ $value }}</option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="col-xs-6 col-sm-6 col-md-6">
+                                        <div class="form-group">
+                                            <strong>User:</strong>
+                                            <select class="form-control" id="user_id" name="user_id">
+                                                <option value="">Select User</option>
+                                                @foreach($user as $key => $value)
+                                                    <option value="{{ $key }}">{{ $value }}</option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="col-xs-6 col-sm-6 col-md-6">
+                                        <div class="form-group">
+                                            <strong>Booth Latitude:</strong>
+                                            {!! Form::text('latitude', null, array('placeholder' => 'ASMB Code','class' => 'form-control')) !!}
+                                        </div>
+                                    </div>
+                                    <div class="col-xs-6 col-sm-6 col-md-6">
+                                        <div class="form-group">
+                                            <strong>Booth Longitude</strong>
+                                            {!! Form::text('longitude', null, array('placeholder' => 'ASMB Code','class' => 'form-control')) !!}
+                                        </div>
+                                    </div>
+                                    <div class="col-xs-6 col-sm-6 col-md-6">
+                                        <div class="form-group">
+                                            <strong>Status:</strong>
+                                            <select class="form-control" name="status">
+                                                <option value="1">ON</option>
+                                                <option value="0">OFF</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="col-xs-6 col-sm-6 col-md-6">
+                                        <button type="submit" class="btn btn-primary">Submit</button>
+                                    </div>
+                                </div>
+                            {!! Form::close() !!}
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <x-footers.auth></x-footers.auth>
+        </div>
+    </main>
+    <x-plugins></x-plugins>
+</x-layout>
+<script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+<script>
+    document.getElementById('state_id').addEventListener('change', function() {
+        var selectedOption = this.value;
+        jQuery('#district_id').find('option').not(':first').remove();
+        // Make an AJAX request
+        axios.get('{{ route('assemblies.getStates') }}', {
+            params: {
+                selectedOption: selectedOption
+            }
+        })
+        .then(function(response) {
+            console.log(response.data);
+            // Iterate through the response and append data to the container
+            response.data.forEach(function(value) {
+                jQuery('#district_id').append(jQuery('<option>').val(value.id).text(value.name))
+            });
+        })
+        .catch(function(error) {
+            console.error(error);
+        });
+    });
+</script>
