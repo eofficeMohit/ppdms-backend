9162d970a3ff842c340b5dc6f4b3c95bf97f73cf
webadmin <webadmin@ELECTIONWEB2.DGR.punjab.gov.in>
2023-08-29 15:29:50 +0530

    changes for login footer

--- public/sendsms.php
+++ public/sendsms.php
@@ -2,7 +2,7 @@
 
 function get_content_GET($url,$data)
 {
-	echo "here comes";
+	
     $ch = curl_init();
     curl_setopt ($ch, CURLOPT_URL, $url);
     curl_setopt ($ch, CURLOPT_HEADER, 0);
@@ -17,6 +17,7 @@ function get_content_GET($url,$data)
     else $curl_output=curl_exec($ch);
     curl_close($ch);   
 
+echo "here comes 2";
 
 
  
@@ -27,16 +28,21 @@ function get_content_GET($url,$data)
     //ob_end_clean();
     print_r($curl_output);
     //return $string;     
+
+echo $curl_output."\n";
    return $curl_output;
 }
-$msg ="Punjab Bye Elections 2023 to 04-Jalandhar(SC) PC, Dear Sector Officer, Your OTP for registration on PPDMS Mobile App is 1234. -CEOPJB";
-$recNo = "9646300130";
-$template_id="1407168318422038309";
+//$msg ="Punjab Bye Elections 2023 to 04-Jalandhar(SC) PC, Dear Sector Officer, Your OTP for registration on PPDMS Mobile App is 1234. -CEOPJB";
+$recNo = "9888933280";
+//$template_id="1407168318422038309";
+$template_id="1407169026079836976";
+
+$msg="This is a test message. Please ignore the same.";
 $data="username=pbgovt.sms&pin=wbx3actu&message=".$msg."&mnumber=".$recNo."&dlt_template_id=".$template_id."&signature=PBGOVT&msgType=UC&dlt_entity_id=1301157492438182299";
 $url="https://smsgw.sms.gov.in/failsafe/HttpLink?";
 
 $content = get_content_GET($url,$data);
 echo $content."\n";
-die('here123');
+
 ?>
 
