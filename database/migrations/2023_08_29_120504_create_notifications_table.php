0161b48433c91f796ed9165892a9d3d51b37effd
eofficeMohit <eoffice.mohit@gmail.com>
2023-08-29 14:58:51 +0530

    new migrations and models created

--- database/migrations/2023_08_29_120504_create_notifications_table.php
+++ database/migrations/2023_08_29_120504_create_notifications_table.php
@@ -13,7 +13,20 @@ return new class extends Migration
     {
         Schema::create('notifications', function (Blueprint $table) {
             $table->id();
+            $table->string('title')->nullable();
+            $table->string('sender_id')->nullable();
+            $table->string('receiver_id')->nullable();
+            $table->string('module_id')->nullable();
+            $table->unsignedBigInteger('user_id');
+            $table->string('url')->nullable();
+            $table->string('notification_type')->nullable();
+            $table->string('message')->nullable();
+            $table->string('sender_status')->nullable();
+            $table->string('receiver_status')->nullable();
+            $table->string('seen')->default(0)->nullable();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
