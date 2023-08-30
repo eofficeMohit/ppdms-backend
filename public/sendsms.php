4a9a23098557717d98874b1c13c5d77fe21f5d8f
webadmin <webadmin@ELECTIONWEB2.DGR.punjab.gov.in>
2023-08-30 18:04:46 +0530

    changes for api and events

--- public/sendsms.php
+++ public/sendsms.php
@@ -11,38 +11,72 @@ function get_content_GET($url,$data)
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER,false);
     curl_setopt($ch, CURLOPT_SSL_VERIFYHOST,2);
-    curl_setopt($ch, CURLOPT_CAINFO,'/etc/pki/tls/certs/ca-bundle.crt');
+    //curl_setopt($ch, CURLOPT_CAINFO,'/etc/pki/tls/certs/ca-bundle.crt');
     //ob_start();
     if (curl_errno($ch)) echo 'Curl error: ' . curl_error($ch);
     else $curl_output=curl_exec($ch);
     curl_close($ch);   
 
-echo "here comes 2";
+echo $curl_output."\n";
 
 
  
-   // ob_start();
-    //curl_exec ($ch);
-    //curl_close ($ch);
-    //$string = ob_get_contents();
-    //ob_end_clean();
     print_r($curl_output);
     //return $string;     
 
 echo $curl_output."\n";
    return $curl_output;
 }
-//$msg ="Punjab Bye Elections 2023 to 04-Jalandhar(SC) PC, Dear Sector Officer, Your OTP for registration on PPDMS Mobile App is 1234. -CEOPJB";
-$recNo = "9888933280";
-//$template_id="1407168318422038309";
-$template_id="1407169026079836976";
 
-$msg="This is a test message. Please ignore the same.";
-$data="username=pbgovt.sms&pin=wbx3actu&message=".$msg."&mnumber=".$recNo."&dlt_template_id=".$template_id."&signature=PBGOVT&msgType=UC&dlt_entity_id=1301157492438182299";
+
+     function THex($str) 
+        {
+        	$unicode = array();
+	        $values = array();
+        	$lookingFor = 1;
+        	for ($i = 0; $i < strlen($str); $i++) 
+                {
+            		$thisValue = ord($str[$i]);
+		            if ($thisValue < 128) 
+                            {
+                		$number = dechex($thisValue);
+		                $unicode[] = (strlen($number) == 1) ? '%u000' . $number : "%u00" . $number;
+            		    } 
+                            else 
+                            {
+               			if (count($values) == 0)
+		                $lookingFor = ( $thisValue < 224 ) ? 2 : 3;
+		                $values[] = $thisValue;
+                		if (count($values) == $lookingFor) 
+				{
+                    			$number = ( $lookingFor == 3 ) ?
+                           		( ( $values[0] % 16 ) * 4096 ) + ( ( $values[1] % 64 ) * 64 ) + ( $values[2] % 64 ) :
+		                        ( ( $values[0] % 32 ) * 64 ) + ( $values[1] % 64);
+                    			$number = dechex($number);
+                    			$unicode[] =(strlen($number) == 3) ? "%u0" . $number : "%u" . $number;
+                    			$values = array();
+		                        $lookingFor = 1;
+                		} // if
+            		    } // if
+                }
+        	return implode("", $unicode);
+        }
+
+$msg ="Dear Vatan Garg ,your letter has been received by Others . For future communications please refer to receipt no. 1802150/2022/Div7. Regards, PANKAJ JAIN , You can track your Medical bills at https://hrms.punjab.gov.in, eOffice Punjab State.";
+$msg = str_replace('%u', '',THex($msg));
+$recNo = "917888379164";
+
+$template_id="1407165174274928332";
+
+
+ $str="username=pbgovt.sms&pin=wbx3actu&message=".$msg."&mnumber=".$recNo."&dlt_template_id=".$template_id."&signature=PBGOVT&msgType=UC&dlt_entity_id=1301157492438182299";
 $url="https://smsgw.sms.gov.in/failsafe/HttpLink?";
 
-$content = get_content_GET($url,$data);
-echo $content."\n";
+echo "\n".$str."\n";
+ echo "\n".$url."\n";
+
+	$content = get_content_GET($str,$url);
+	echo $content."\n";
 
 ?>
 
