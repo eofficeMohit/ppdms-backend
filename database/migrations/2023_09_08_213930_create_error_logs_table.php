2e79291931c9e299eabddabefe958bc117278d21
rishavdeepsran <142724574+rishavdeepsran@users.noreply.github.com>
2023-09-08 22:35:25 +0530

    Save logs in daily basis file and in database table name error_logs

---
+++ database/migrations/2023_09_08_213930_create_error_logs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('error_logs', function (Blueprint $table) {
+            $table->id();
+			$table->unsignedBigInteger('user_id');
+            $table->string('code');
+            $table->string('file');
+            $table->string('line');
+            $table->text('message');
+            $table->text('trace');
+			$table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('error_logs');
+    }
+};
