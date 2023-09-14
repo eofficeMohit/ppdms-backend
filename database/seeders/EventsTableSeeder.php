f5dfe985ad3243702fff77460739f14b3b47d057
eofficeMohit <eoffice.mohit@gmail.com>
2023-09-14 16:44:38 +0530

    chnages for seeder

--- database/seeders/EventsTableSeeder.php
+++ database/seeders/EventsTableSeeder.php
@@ -4,29 +4,28 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\Models\Parliament;
-
-class ParliamentTableSeeder extends Seeder
+use App\Models\Event;
+class EventsTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-
         \Schema::disableForeignKeyConstraints();
-        Parliament::truncate();
+        Event::truncate();
         \Schema::enableForeignKeyConstraints();
-        $csvData = fopen(base_path('database/csv/parliaments.csv'), 'r');
+        $csvData = fopen(base_path('database/csv/events.csv'), 'r');
         $transRow = true;
         while (($data = fgetcsv($csvData, 555, ',')) !== false) {
             if (!$transRow) {
-                Parliament::create([
+                Event::create([
                     'id' => $data['0'],
-                    'pc_no' =>$data['1'],
-                    'pc_name' =>$data['2'],
-                    'pc_type' =>$data['3'],
-                    'state_id' =>$data['4'],
+                    'event_name' => $data['1'],
+                    'event_sequence' => $data['2'],
+                    'start_date_time' => $data['3'],
+                    'end_date_time' => $data['4'],
+                    'status' => $data['5'],
                     'created_at' => now(),
                     'updated_at' => now(),
                 ]);
