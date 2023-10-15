c1ba2b99c58d08fe8fef770e5ca50552667bc35f
Mohit Jamwal <mohitjamwal@Mohits-MacBook.local>
2023-10-15 21:53:27 +0530

    changes for portals

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -9,16 +9,18 @@ use App\Models\ElectionInfo;
 class DashboardController extends Controller
 {
     public function index(Request $request)
-    {   
-        $electionInfo = ElectionInfo::with('electionState','electionDistrict','electionBooth','electionAssembly','electionEvent')->latest()->paginate(20);
-        $total_party_dispatch = ElectionInfo::where('event_id',1)->count();
-        $total_party_reached = ElectionInfo::where('event_id',2)->count();
-        $total_mock_poll_started = ElectionInfo::where('event_id',3)->count();
-        return view('dashboard.index',compact('electionInfo','total_party_dispatch','total_party_reached','total_mock_poll_started'))->with('i', ($request->input('page', 1) - 1) * 20);
+    {
+        $electionInfo = ElectionInfo::with('electionState', 'electionDistrict', 'electionBooth', 'electionAssembly', 'electionEvent')
+            ->latest()
+            ->paginate(20);
+        $total_party_dispatch = ElectionInfo::where('event_id', 1)->count();
+        $total_party_reached = ElectionInfo::where('event_id', 2)->count();
+        $total_mock_poll_started = ElectionInfo::where('event_id', 3)->count();
+        return view('dashboard.index', compact('electionInfo', 'total_party_dispatch', 'total_party_reached', 'total_mock_poll_started'))->with('i', ($request->input('page', 1) - 1) * 20);
     }
     public function indexStat()
     {
-        $locations = Booth::select('latitude','longitude','booth_name','status')->get();
-        return view('dashboard.indexStat',compact('locations'));
+        $locations = Booth::select('latitude', 'longitude', 'booth_name', 'assigned_status')->get();
+        return view('dashboard.indexStat', compact('locations'));
     }
 }
--- resources/views/dashboard/indexStat.blade.php
+++ resources/views/dashboard/indexStat.blade.php
@@ -1,6 +1,5 @@
-
 <x-layout bodyClass="g-sidenav-show  bg-gray-200">
-{{-- {{dd($locations)}} --}}
+    {{-- {{dd($locations)}} --}}
     <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
         <!-- Navbar -->
         <x-navbars.navs.auth titlePage="BoothWatch Live"></x-navbars.navs.auth>
@@ -16,208 +15,180 @@
     <x-plugins></x-plugins>
     </div>
     @push('js')
+        <script src="https://maps.googleapis.com/maps/api/js?key={{ env('GOOGLE_MAP_API_KEY') }}&callback=initMap" async defer>
+        </script>
+        <script type="text/javascript">
+            const apiKey = '{{ env('GOOGLE_MAP_API_KEY') }}';
+            var locations = @json($locations);
+            var center = locations[0];
 
+            const customMapStyles = [{
+                    "featureType": "administrative.province",
+                    "elementType": "all",
+                    "stylers": [{
+                        "visibility": "on"
+                    }]
+                },
+                {
+                    "featureType": "administrative.locality",
+                    "elementType": "all",
+                    "stylers": [{
+                        "visibility": "off"
+                    }]
+                },
+                {
+                    "featureType": "administrative.neighborhood",
+                    "elementType": "all",
+                    "stylers": [{
+                        "visibility": "on"
+                    }]
+                },
+                {
+                    "featureType": "landscape.natural",
+                    "elementType": "geometry.fill",
+                    "stylers": [{
+                            "visibility": "on"
+                        },
+                        {
+                            "color": "#e0efef"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "poi",
+                    "elementType": "geometry.fill",
+                    "stylers": [{
+                            "visibility": "on"
+                        },
+                        {
+                            "hue": "#1900ff"
+                        },
+                        {
+                            "color": "#c0e8e8"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "poi.attraction",
+                    "elementType": "all",
+                    "stylers": [{
+                        "visibility": "off"
+                    }]
+                },
+                {
+                    "featureType": "poi.business",
+                    "elementType": "all",
+                    "stylers": [{
+                        "visibility": "off"
+                    }]
+                },
+                {
+                    "featureType": "poi.government",
+                    "elementType": "all",
+                    "stylers": [{
+                        "visibility": "on"
+                    }]
+                },
+                {
+                    "featureType": "poi.medical",
+                    "elementType": "all",
+                    "stylers": [{
+                        "visibility": "off"
+                    }]
+                },
+                {
+                    "featureType": "poi.park",
+                    "elementType": "all",
+                    "stylers": [{
+                        "visibility": "off"
+                    }]
+                },
+                {
+                    "featureType": "poi.place_of_worship",
+                    "elementType": "all",
+                    "stylers": [{
+                        "visibility": "off"
+                    }]
+                },
+                {
+                    "featureType": "poi.school",
+                    "elementType": "all",
+                    "stylers": [{
+                        "visibility": "on"
+                    }]
+                },
+                {
+                    "featureType": "poi.sports_complex",
+                    "elementType": "all",
+                    "stylers": [{
+                        "visibility": "off"
+                    }]
+                },
+                {
+                    "featureType": "road",
+                    "elementType": "geometry",
+                    "stylers": [{
+                            "lightness": 100
+                        },
+                        {
+                            "visibility": "simplified"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "road",
+                    "elementType": "labels",
+                    "stylers": [{
+                        "visibility": "off"
+                    }]
+                },
+                {
+                    "featureType": "transit.line",
+                    "elementType": "geometry",
+                    "stylers": [{
+                            "visibility": "on"
+                        },
+                        {
+                            "lightness": 700
+                        }
+                    ]
+                },
+                {
+                    "featureType": "water",
+                    "elementType": "all",
+                    "stylers": [{
+                        "color": "#7dcdcd"
+                    }]
+                }
+            ]
 
-    <script src="https://maps.googleapis.com/maps/api/js?key={{ env('GOOGLE_MAP_API_KEY') }}&callback=initMap" async
-    defer></script>
-  <script type="text/javascript">
-  const apiKey = '{{ env('GOOGLE_MAP_API_KEY') }}';
-  var locations = <?php print_r(json_encode($locations)) ?>;
+            function initMap() {
+                const mapOptions = {
+                    center: {
+                        lat: 31.3260,
+                        lng: 75.5762
+                    },
+                    zoom: 8,
+                    styles: customMapStyles
+                };
 
+                const map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
-  const customMapStyles = [
-    {
-        "featureType": "administrative.province",
-        "elementType": "all",
-        "stylers": [
-            {
-                "visibility": "on"
-            }
-        ]
-    },
-    {
-        "featureType": "administrative.locality",
-        "elementType": "all",
-        "stylers": [
-            {
-                "visibility": "off"
-            }
-        ]
-    },
-    {
-        "featureType": "administrative.neighborhood",
-        "elementType": "all",
-        "stylers": [
-            {
-                "visibility": "on"
-            }
-        ]
-    },
-    {
-        "featureType": "landscape.natural",
-        "elementType": "geometry.fill",
-        "stylers": [
-            {
-                "visibility": "on"
-            },
-            {
-                "color": "#e0efef"
-            }
-        ]
-    },
-    {
-        "featureType": "poi",
-        "elementType": "geometry.fill",
-        "stylers": [
-            {
-                "visibility": "on"
-            },
-            {
-                "hue": "#1900ff"
-            },
-            {
-                "color": "#c0e8e8"
-            }
-        ]
-    },
-    {
-        "featureType": "poi.attraction",
-        "elementType": "all",
-        "stylers": [
-            {
-                "visibility": "off"
-            }
-        ]
-    },
-    {
-        "featureType": "poi.business",
-        "elementType": "all",
-        "stylers": [
-            {
-                "visibility": "off"
-            }
-        ]
-    },
-    {
-        "featureType": "poi.government",
-        "elementType": "all",
-        "stylers": [
-            {
-                "visibility": "on"
-            }
-        ]
-    },
-    {
-        "featureType": "poi.medical",
-        "elementType": "all",
-        "stylers": [
-            {
-                "visibility": "off"
-            }
-        ]
-    },
-    {
-        "featureType": "poi.park",
-        "elementType": "all",
-        "stylers": [
-            {
-                "visibility": "off"
-            }
-        ]
-    },
-    {
-        "featureType": "poi.place_of_worship",
-        "elementType": "all",
-        "stylers": [
-            {
-                "visibility": "off"
-            }
-        ]
-    },
-    {
-        "featureType": "poi.school",
-        "elementType": "all",
-        "stylers": [
-            {
-                "visibility": "on"
-            }
-        ]
-    },
-    {
-        "featureType": "poi.sports_complex",
-        "elementType": "all",
-        "stylers": [
-            {
-                "visibility": "off"
-            }
-        ]
-    },
-    {
-        "featureType": "road",
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "lightness": 100
-            },
-            {
-                "visibility": "simplified"
-            }
-        ]
-    },
-    {
-        "featureType": "road",
-        "elementType": "labels",
-        "stylers": [
-            {
-                "visibility": "off"
-            }
-        ]
-    },
-    {
-        "featureType": "transit.line",
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "visibility": "on"
-            },
-            {
-                "lightness": 700
-            }
-        ]
-    },
-    {
-        "featureType": "water",
-        "elementType": "all",
-        "stylers": [
-            {
-                "color": "#7dcdcd"
-            }
-        ]
-    }
-]
+                // Add markers for each location with different colors
+                for (const location of locations) {
+                    const marker = new google.maps.Marker({
+                        position: {
+                            lat: location.latitude,
+                            lng: location.longitude
+                        },
+                        map: map,
+                        title: location.booth_name,
+                        icon: location.assigned_status ? "http://maps.google.com/mapfiles/ms/icons/blue.png" :
+                            "http://maps.google.com/mapfiles/ms/icons/red.png"
 
-  function initMap() {
-    const mapOptions = {
-      center: { lat: 31.3260, lng: 75.5762 },
-      zoom:7.5,
-      styles: customMapStyles
-    };
-
-    const map = new google.maps.Map(document.getElementById('map'), mapOptions);
-
-    // Add markers for each location with different colors
-    for (const location of locations) {
-      const marker = new google.maps.Marker({
-        position: { lat: location.latitude, lng: location.longitude },
-        map: map,
-        title: location.booth_name,
-        icon: location.status ? "http://maps.google.com/mapfiles/ms/icons/red.png" : "http://maps.google.com/mapfiles/ms/icons/blue.png"
-
-      });
-    }
-  }
-</script>
-
-
+                    });
+                }
+            }
+        </script>
     @endpush
 </x-layout>
